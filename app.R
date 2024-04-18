59a4e4bba013b7bfe520f0e8621b159800eb9090
Barret Schloerke <barret@posit.co>
2024-04-17 22:39:25 -0400

    update app to latest old faithful

--- app.R
+++ app.R
@@ -1,9 +1,55 @@
-if (FALSE) {
-  library(shiny)
-  library(bslib)
-  library(plotly)
-  library(colourpicker)
-  library(bsicons)
+# Direct copy of https://github.com/rstudio/shiny/blob/3edf9bfad8ad69a93f744a4f63447442d23e7c9a/inst/examples-shiny/01_hello/app.R
+
+library(shiny)
+library(bslib)
+
+# Define UI for app that draws a histogram ----
+ui <- page_sidebar(
+
+  # App title ----
+  title = "Hello Shiny!",
+
+  # Sidebar panel for inputs ----
+  sidebar = sidebar(
+
+    # Input: Slider for the number of bins ----
+    sliderInput(
+      inputId = "bins",
+      label = "Number of bins:",
+      min = 1,
+      max = 50,
+      value = 30
+    )
+  ),
+
+  # Output: Histogram ----
+  plotOutput(outputId = "distPlot")
+)
+
+# Define server logic required to draw a histogram ----
+server <- function(input, output) {
+  # Histogram of the Old Faithful Geyser Data ----
+  # with requested number of bins
+  # This expression that generates a histogram is wrapped in a call
+  # to renderPlot to indicate that:
+  #
+  # 1. It is "reactive" and therefore should be automatically
+  #    re-executed when inputs (input$bins) change
+  # 2. Its output type is a plot
+  output$distPlot <- renderPlot({
+    x <- faithful$waiting
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+    hist(
+      x,
+      breaks = bins,
+      col = "#75AADB",
+      border = "white",
+      xlab = "Waiting time to next eruption (in mins)",
+      main = "Histogram of waiting times"
+    )
+  })
 }
 
-shiny::shinyAppDir(system.file("examples/build-a-box", package = "bslib"))
+# Create Shiny app ----
+shinyApp(ui = ui, server = server)
